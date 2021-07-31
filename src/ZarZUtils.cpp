a831831eae30b3976c068b1483326e8f2fc47807
Ivan Zagorodny <ivanzagorodny2003@gmail.com>
2021-07-31 03:55:08 -0300

    first

---
+++ src/ZarZUtils.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <ZarZUtils.h>
+#include <string.h>
+#include <stdlib.h>
+
+ZarZUtils::ZarZUtils()
+
+
+String ZarZUtils::Input()
+{
+  char a=Serial.read();
+  String aa="";
+  while(a!=-1 ){
+    
+    aa= aa + a;
+
+    a = Serial.read();
+    return(aa);
+    }
+}
---
+++ src/ZarZUtils.h
@@ -0,0 +1,19 @@
+#ifndef ZarZUtils_h
+#define ZarZUtils_h
+#include <Arduino.h>
+#include <SPI.h>
+
+
+
+class RC522_RFID_Utilities
+{
+public:
+	String Input();
+
+
+	};
+
+
+
+
+#endif
